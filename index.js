55beb9e75d5a4865fe280161c7950ee9e796c040
Achieve-software <vtcbcdtyr@gmail.com>
2023-06-04 20:41:54 +0300

    mdl

---
+++ index.js
@@ -0,0 +1,6 @@
+//İNDEX.JS
+import { topla } from "./mymodule.js";
+import { PI } from "./mymodule.js";
+
+console.log("TOPLAM:", topla(2, 3));
+console.log(PI);
---
+++ mymodule.js
@@ -0,0 +1,5 @@
+//MYMODULE.JS
+export function topla(s1, s2) {
+  return s1 + s2;
+}
+export const PI = 3.14;
